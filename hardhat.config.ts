46d258b8cc7912ce337e23d25a9b8b6215012f88
Muhammet Ali AKBAY <maakbay@blockchains.com>
2021-05-06 23:50:41 +0300

    Configured hardhat and typescript

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import {task} from "hardhat/config";
+import '@nomiclabs/hardhat-waffle';
+
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+};
