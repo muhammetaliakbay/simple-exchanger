19c2878e6d6bdf5e7d8edd8310c3f1d097577554
Muhammet Ali AKBAY <maakbay@blockchains.com>
2021-05-08 07:24:10 +0300

    Replaced "number" parameter types with "BigNumberish" in SortedTest type definition

--- contracts/sorted-test.d.ts
+++ contracts/sorted-test.d.ts
@@ -1,9 +1,10 @@
 import {Contract} from "ethers";
+import {BigNumberish} from "@ethersproject/bignumber";
 
 export interface SortedTest extends Contract {
-    length(): Promise<number>
+    length(): Promise<BigNumberish>
     all(): Promise<string[]>
-    insert(rank: number, value: string, asc: boolean): Promise<void>
+    insert(rank: BigNumberish, value: string, asc: boolean): Promise<void>
     removeFirst(): Promise<void>
-    removeAt(offset: number): Promise<void>
+    removeAt(offset: BigNumberish): Promise<void>
 }
