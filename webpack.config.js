868bf035bbe8482b2ddaf1490bebfd80074d1887
Muhammet Ali AKBAY <maakbay@blockchains.com>
2021-05-11 08:47:16 +0300

    Added webpack.config.js and updated tsconfig.json regarding the structure

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './app/index.tsx',
+    devtool: 'inline-source-map',
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.json']
+    },
+    output: {
+        publicPath: '/',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'app/index.html'),
+            filename: 'index.html'
+        })
+    ]
+}
