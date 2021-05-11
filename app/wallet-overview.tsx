ad4b5c7f16c88b72ca033ed640c1451af31fe791
Muhammet Ali AKBAY <maakbay@blockchains.com>
2021-05-11 09:09:34 +0300

    WalletOverview component which shows information about tokens claimed by the account

---
+++ app/wallet-overview.tsx
@@ -0,0 +1,71 @@
+import {Wallet} from "../client/wallet";
+import {Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import React from "react";
+import {StableTokenAccount, StableTokenClient} from "../client/stable-token";
+import usePromise from "react-use-promise";
+import {useObservable} from "react-use-observable";
+import {Link, useRouteMatch} from "react-router-dom";
+import {toFixedPointString} from "./amount-utils";
+
+export function WalletOverview(
+    {
+        wallet,
+        tokens
+    }: {
+        wallet: Wallet,
+        tokens: StableTokenClient[]
+    }
+) {
+    const address = wallet.getAddress();
+    const accounts = tokens.map(
+        token => token.getAccount(address)
+    )
+
+    return <TableContainer component={Paper}>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Currency</TableCell>
+                    <TableCell>Available</TableCell>
+                    <TableCell>Locked</TableCell>
+                    <TableCell>Total</TableCell>
+                    <TableCell />
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {accounts.map(
+                    account => <AccountRow key={account.client.getContractAddress()} account={account} />
+                )}
+            </TableBody>
+        </Table>
+    </TableContainer>
+}
+
+function AccountRow(
+    {
+        account
+    }: {
+        account: StableTokenAccount
+    }
+) {
+    const {url} = useRouteMatch();
+
+    const token = account.client;
+    const [currency] = usePromise(() => token.getCurrency(), [token])
+    const [balance] = useObservable(() => account.balance$, [account])
+
+    return <TableRow>
+        <TableCell>{currency?.code}</TableCell>
+        <TableCell>{balance && currency && toFixedPointString(balance.available, currency.precision)}</TableCell>
+        <TableCell>{balance && currency && toFixedPointString(balance.locked, currency.precision)}</TableCell>
+        <TableCell>{balance && currency && toFixedPointString(balance.available.add(balance.locked), currency.precision)}</TableCell>
+        <TableCell>
+            <Link to={`${url}/${token.getContractAddress()}/deposit`}>
+                <Button>Deposit</Button>
+            </Link>
+            <Link to={`${url}/${token.getContractAddress()}/withdraw`}>
+                <Button>Withdraw</Button>
+            </Link>
+        </TableCell>
+    </TableRow>
+}
