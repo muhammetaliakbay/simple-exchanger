af8dd364200f56e374061c54bf0a0af098de38ed
Muhammet Ali AKBAY <maakbay@blockchains.com>
2021-05-11 17:29:12 +0300

    Parallel contract deployment implementation

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,32 +1,76 @@
 import { ethers } from "hardhat";
 import stableCurrencies from "../stable-currencies.json";
 import {deployStableToken} from "../deployment/stable-token";
-import {saveRuntimeData} from "../deployment/runtime";
-import {ExchangerDefinition} from "../instances/definitions";
 import {deployExchanger} from "../deployment/exchanger";
 import {deployOrderBook} from "../deployment/order-book";
 import {deployOrderLibrary} from "../deployment/order";
+import {SYNC} from "../deployment/sync";
+import {promisify} from "util";
+import {writeFile} from "fs";
+import {DeploymentInfo} from "../deployment/deployment-info";
+import {getAddress} from "./task-utils";
 
 export default async function() {
 
+    const queue: Promise<any>[] = [Promise.resolve()];
+
     console.log("Deploying Order")
-    const Order = await deployOrderLibrary(ethers);
+    const Order$ = deployOrderLibrary(ethers);
 
     console.log("Deploying Exchanger")
-    const exchanger = await deployExchanger(ethers);
-    saveRuntimeData(ExchangerDefinition, exchanger)
+    const exchanger$ = deployExchanger(ethers);
 
     for (const currency of stableCurrencies) {
 
         console.log("Deploying StableToken", currency)
-        const stableToken = await deployStableToken(ethers, currency, 2);
-        console.log("Deploying OrderBook")
-        const orderBook = await deployOrderBook(ethers, stableToken, 18, {Order});
-        console.log("Registering OrderBook as manager of StableToken")
-        await (await stableToken.addManager(orderBook.address)).wait()
+        const stableToken$ = deployStableToken(ethers, currency, 2);
 
-        console.log("Registering OrderBook", orderBook.address)
-        await (await (exchanger.registerOrderBook(orderBook.address))).wait();
+        const orderBook$ = Promise.all([stableToken$, Order$]).then(
+            ([stableToken, Order]) => {
+                console.log("Deploying OrderBook", currency)
+                return deployOrderBook(ethers, stableToken, 18, {Order})
+            }
+        )
+
+        queue.push(
+            Promise.all([stableToken$, orderBook$]).then(
+                ([stableToken, orderBook]) => {
+                    console.log("Registering OrderBook as manager of StableToken", orderBook.address, currency)
+                    return SYNC.runTx(
+                        () => stableToken.addManager(orderBook.address)
+                    )
+                }
+            )
+        )
+
+        queue.push(
+            Promise.all([exchanger$, orderBook$]).then(
+                ([exchanger, orderBook]) => {
+                    console.log("Registering OrderBook", orderBook.address, currency)
+                    return SYNC.runTx(
+                        () => exchanger.registerOrderBook(orderBook.address)
+                    )
+                }
+            )
+        )
     }
 
+    await Promise.all(queue).then(
+        async () => {
+            const exchanger = await exchanger$;
+            const account = await getAddress(0);
+            const network = await ethers.provider.getNetwork()
+            const info: DeploymentInfo = {
+                network: {
+                    name: network.name,
+                    chainId: network.chainId
+                },
+                account: account,
+                exchangeAddress: exchanger.address
+            }
+            console.log("Deployment is completed.", info)
+            console.log("Saving deployment information")
+            await promisify(writeFile)('deployment.json', JSON.stringify(info, undefined, 3))
+        }
+    );
 }
